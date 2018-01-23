1aed5e9a89f7c464722d3f85625e8ad7da359ee4
Marc Henrard <marc@opengamma.com>
2018-01-23 11:36:38 +0100

    Added correlation to timeseries utils.

--- src/main/java/marc/henrard/murisq/basics/data/timeseries/LocalDateDoubleTimeSeriesUtils.java
+++ src/main/java/marc/henrard/murisq/basics/data/timeseries/LocalDateDoubleTimeSeriesUtils.java
@@ -7,6 +7,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.opengamma.strata.collect.ArgChecker;
 import com.opengamma.strata.collect.timeseries.LocalDateDoublePoint;
 import com.opengamma.strata.collect.timeseries.LocalDateDoubleTimeSeries;
 
@@ -18,9 +19,10 @@ import com.opengamma.strata.collect.timeseries.LocalDateDoubleTimeSeries;
 public class LocalDateDoubleTimeSeriesUtils {
   
   /**
-   * Computes the daily volatility of a time series. The volatility is computed as the square root
-   * of the sum of the squares of the differences between consecutive points divided by the number of
-   * days between the first and last point:
+   * Computes the daily volatility of a time series. 
+   * <p>
+   * The volatility is computed as the square root of the sum of the squares of the differences 
+   * between consecutive points divided by the number of days between the first and last point:
    *   v = sqrt( sum( (v_i+1 - v_i)^2 ) / d)
    *   d = timeSeries.getLatestDate() - timeSeries.getEarliestDate()
    * 
@@ -37,6 +39,39 @@ public class LocalDateDoubleTimeSeriesUtils {
       vol2 += change * change;
     }
     return Math.sqrt(vol2 / nbDays);
+  }
+  
+  /**
+   * Computes the correlation of a two time series. 
+   * <p>
+   * The correlation is computed as the square root
+   * of the sum of the squares of the differences between consecutive points divided by the number of
+   * days between the first and last point:
+   *   delta_{j,i} = v_{j,i+1} - v_{j,i}  j=1,2
+   *   var_j = sum_i(delta_{j,i}^2)
+   *   rho = sum_i(delta_{1,i}*delta_{2,i}) / sqrt(var_1 * var_2)
+   * 
+   * @param timeSeries  the time series
+   * @return the correlation
+   */
+  public static double correlation(
+      LocalDateDoubleTimeSeries timeSeries1, 
+      LocalDateDoubleTimeSeries timeSeries2) {
+    List<LocalDateDoublePoint> points1 = timeSeries1.stream().collect(Collectors.toList());
+    List<LocalDateDoublePoint> points2 = timeSeries2.stream().collect(Collectors.toList());
+    int nbPts = points1.size();
+    ArgChecker.isTrue(points2.size() == nbPts, "time series must have the same length");
+    double var_1 = 0.0;
+    double var_2 = 0.0;
+    double cov = 0.0;
+    for (int looppt = 0; looppt < nbPts - 1; looppt++) {
+      double change1 = points1.get(looppt + 1).getValue() - points1.get(looppt).getValue();
+      double change2 = points2.get(looppt + 1).getValue() - points2.get(looppt).getValue();
+      var_1 += change1 * change1;
+      var_2 += change2 * change2;
+      cov += change1 * change2;
+    }
+    return cov / Math.sqrt(var_1 * var_2);
   }
 
 }
