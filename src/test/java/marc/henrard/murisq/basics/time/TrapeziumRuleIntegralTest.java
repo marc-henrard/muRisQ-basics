295411c66c90cb70739db493702e691e498d386d
Marc Henrard <marc@opengamma.com>
2019-11-21 13:13:08 +0100

    Update to Strata 2.6. Start switch to JUnit.

--- src/test/java/marc/henrard/murisq/basics/time/TrapeziumRuleIntegralTest.java
+++ src/test/java/marc/henrard/murisq/basics/time/TrapeziumRuleIntegralTest.java
@@ -3,8 +3,8 @@
  */
 package marc.henrard.murisq.basics.time;
 
-import static com.opengamma.strata.collect.TestHelper.assertThrowsIllegalArg;
-import static org.testng.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -12,7 +12,8 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-import org.testng.annotations.Test;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableList;
 import com.opengamma.strata.collect.array.DoubleArray;
@@ -22,7 +23,6 @@ import com.opengamma.strata.collect.array.DoubleArray;
  * 
  * @author Marc Henrard
  */
-@Test
 public class TrapeziumRuleIntegralTest {
 
   private final static ScaledSecondTime TIME_MEASURE = ScaledSecondTime.DEFAULT;
@@ -33,9 +33,10 @@ public class TrapeziumRuleIntegralTest {
   private final static TrapeziumRuleIntegral TRAPEZIUM_RULE =
       TrapeziumRuleIntegral.of(TIME_MEASURE, VALUATION_DATE);
   
-  private final static double TOLERANCE_INT = 1.0E-8;
+  private final static Offset<Double> TOLERANCE_INT = Offset.offset(1.0E-8);
   
   /* Compare trapezium rule integral to a local implementation */
+  @Test
   public void amalgamateDates_localDate() {
     DoubleArray values = DoubleArray.of(1.0, 4.0, 2.0, 3.0);
     List<LocalDate> dates = ImmutableList.of(
@@ -52,11 +53,12 @@ public class TrapeziumRuleIntegralTest {
           * (times[loopdates+1] - times[loopdates]);
     }
     double integralComputed = TRAPEZIUM_RULE.amalgamateDates(values, dates);
-    assertEquals(integralComputed, integralExpected, TOLERANCE_INT);
+    assertThat(integralComputed).isCloseTo(integralExpected, TOLERANCE_INT);
   }
 
   
   /* Compare trapezium rule running figures to a re-computation of the sub-array */
+  @Test
   public void amalgamateDates_localDate_running() {
     DoubleArray values = DoubleArray.of(1.0, 4.0, 2.0, 3.0);
     List<LocalDate> dates = ImmutableList.of(
@@ -67,11 +69,12 @@ public class TrapeziumRuleIntegralTest {
     for (int i = 0; i < nbDates; i++) {
       double integralExpected =
           TRAPEZIUM_RULE.amalgamateDates(values.subArray(0, i + 1), dates.subList(0, i + 1));
-      assertEquals(integralComputed.get(i), integralExpected, TOLERANCE_INT, "running " + i);
+      assertThat(integralComputed.get(i)).isCloseTo(integralExpected, TOLERANCE_INT);
     }
   }
   
   /* Compare trapezium rule integral to a local implementation */
+  @Test
   public void amalgamateDates_zonedDateTime() {
     DoubleArray values = DoubleArray.of(1.0, 4.0, 2.0, 3.0);
     List<ZonedDateTime> dates = ImmutableList.of(
@@ -90,10 +93,11 @@ public class TrapeziumRuleIntegralTest {
           * (times[loopdates+1] - times[loopdates]);
     }
     double integralComputed = TRAPEZIUM_RULE.amalgamateZonedDates(values, dates);
-    assertEquals(integralComputed, integralExpected, TOLERANCE_INT);
+    assertThat(integralComputed).isCloseTo(integralExpected, TOLERANCE_INT);
   }
 
   /* Compare trapezium rule running figures to a re-computation of the sub-array */
+  @Test
   public void amalgamateDates_zonedDateTime_running() {
     DoubleArray values = DoubleArray.of(1.0, 4.0, 2.0, 3.0);
     List<ZonedDateTime> dates = ImmutableList.of(
@@ -106,27 +110,29 @@ public class TrapeziumRuleIntegralTest {
     for (int i = 0; i < nbDates; i++) {
       double integralExpected =
           TRAPEZIUM_RULE.amalgamateZonedDates(values.subArray(0, i + 1), dates.subList(0, i + 1));
-      assertEquals(integralComputed.get(i), integralExpected, TOLERANCE_INT, "running " + i);
+      assertThat(integralComputed.get(i)).isCloseTo(integralExpected, TOLERANCE_INT);
     }
   }
 
   /* Check values and dates have same size */
+  @Test
   public void amalgamateDates_wrong_size() {
     DoubleArray values = DoubleArray.of(1.0, 4.0, 2.0, 3.0);
     List<LocalDate> dates = ImmutableList.of(
         LocalDate.of(2018, 9, 19), LocalDate.of(2018, 10, 20), 
         LocalDate.of(2018, 11, 21));
-    assertThrowsIllegalArg(() -> TRAPEZIUM_RULE.amalgamateDates(values, dates));
+    assertThatIllegalArgumentException().isThrownBy(() -> TRAPEZIUM_RULE.amalgamateDates(values, dates));
   }
 
   /* Check values and dates have same size */
+  @Test
   public void amalgamateDates_wrong_size_2() {
     DoubleArray values = DoubleArray.of(1.0, 4.0, 2.0, 3.0);
     List<ZonedDateTime> dates = ImmutableList.of(
         ZonedDateTime.of(LocalDate.of(2018, 9, 19), TIME, ZONE),
         ZonedDateTime.of(LocalDate.of(2018, 10, 20), TIME, ZONE),
         ZonedDateTime.of(LocalDate.of(2018, 11, 21), TIME, ZONE));
-    assertThrowsIllegalArg(() -> TRAPEZIUM_RULE.amalgamateZonedDates(values, dates));
+    assertThatIllegalArgumentException().isThrownBy(() -> TRAPEZIUM_RULE.amalgamateZonedDates(values, dates));
   }
   
 }
